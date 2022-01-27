497c5c743e4c8af47eb878e2c41f332ae3d03223
soumjovemdanet <96385166+soumjovemdanet@users.noreply.github.com>
2022-01-27 11:26:05 -0300

    Create pro.py

---
+++ pro.py
@@ -0,0 +1,17 @@
+import python 2,3 
+
+print('Seja bem vindos')
+
+print('*******************************')
+print(' Escolhe uma das opçoes abaixo ')
+print('*******************************')      
+
+
+                 1- aultualziar o repositorio
+                 2- fazer calculos
+                 3- sair (ext)
+      
+    input(3)
+    
+    os.system("exit')
+              
